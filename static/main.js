c309977ff0a97dfbb59476133b6291d0231c5909
Даниил <sorokovnin3431@gmail.com>
2026-06-22 13:33:46 +0300

    update

--- static/main.js
+++ static/main.js
@@ -183,3 +183,52 @@ async function sendOrder(){
 
     }
 }
+const slides =
+document.querySelectorAll(".slide");
+
+const dots =
+document.querySelectorAll(".dot");
+
+let currentSlide = 0;
+
+function showSlide(index){
+
+    slides.forEach(slide =>
+        slide.classList.remove("active"));
+
+    dots.forEach(dot =>
+        dot.classList.remove("active"));
+
+    slides[index].classList.add("active");
+    dots[index].classList.add("active");
+}
+
+function nextSlide(){
+
+    currentSlide++;
+
+    if(currentSlide >= slides.length){
+        currentSlide = 0;
+    }
+
+    showSlide(currentSlide);
+}
+
+function prevSlide(){
+
+    currentSlide--;
+
+    if(currentSlide < 0){
+        currentSlide = slides.length - 1;
+    }
+
+    showSlide(currentSlide);
+}
+
+document.querySelector(".next")
+?.addEventListener("click", nextSlide);
+
+document.querySelector(".prev")
+?.addEventListener("click", prevSlide);
+
+setInterval(nextSlide, 5000);
